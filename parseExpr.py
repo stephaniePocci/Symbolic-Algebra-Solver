cb8e5a065a99896014aa9e5648d3a332cdcac1e6
Andy Chen <acchen@cpp.edu>
2024-07-05 22:00:15 -0700

    add python script for calling parseExpr from sympy

---
+++ parseExpr.py
@@ -0,0 +1,6 @@
+﻿from sympy.parsing.sympy_parser import parse_expr
+from sympy.parsing.sympy_parser import standard_transformations, implicit_multiplication_application, convert_xor
+
+def parse_expression(input):
+   transformations = (standard_transformations + (implicit_multiplication_application,convert_xor,))
+   return parse_expr(input, transformations=transformations)
