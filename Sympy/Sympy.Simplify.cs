f2c08e2919327f6d7bebecddba617a7450463ce6
Andy Chen <acchen@cpp.edu>
2024-07-05 22:04:49 -0700

    add missing sympy helper functions, idk why this was ignored by git

---
+++ Sympy/Sympy.Simplify.cs
@@ -0,0 +1,42 @@
+﻿using Python.Runtime;
+
+namespace Sympy
+{
+    public partial class SympyCS
+    {
+        private static readonly PyObject sympy;
+        private static readonly PyObject parseExpr;
+
+        static SympyCS()
+        {
+            sympy = Py.Import("sympy");
+            parseExpr = Py.Import("parseExpr");
+        }
+
+        private static PyObject parse_expr(string input)
+        {
+            var pyargs = new PyString(input);
+            return parseExpr.InvokeMethod("parse_expression", new PyObject[] { pyargs });
+        }
+
+        public static string latex(PyObject input)
+        {
+            PyObject outExpr = sympy.InvokeMethod("latex", new PyObject[] { input }); ;
+            return outExpr.ToString()!;
+        }
+
+        public static string simplify(string input)
+        {
+            var pyargs = parse_expr(input);
+            PyObject outExpr = sympy.InvokeMethod("simplify", pyargs);
+            return latex(outExpr);
+        }
+
+        public static string factor(string input)
+        {
+            var pyargs = parse_expr(input);
+            PyObject outExpr = sympy.InvokeMethod("factor", new PyObject[] { pyargs });
+            return latex(outExpr);
+        } 
+    }
+}
