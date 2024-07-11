f56ce573e6c4212ddeffdfd10c6aeab7f6e4e31f
Andy Chen <acchen@cpp.edu>
2024-07-11 14:09:03 -0700

    check for matching opening and closing parenthesis for input expressions

--- Models/SimpleExpression.cs
+++ Models/SimpleExpression.cs
@@ -1,13 +1,13 @@
 ﻿using Python.Runtime;
 using Symbolic_Algebra_Solver.Utils;
 using Sympy;
+using System.Windows;
 
 namespace Symbolic_Algebra_Solver.Models
 {
     public class SimpleExpression : Observable
     {
         public string InputExpression { get; set; } = string.Empty;
-
         private string _OutputExpression = string.Empty;
         public string OutputExpression
         {
@@ -19,8 +19,42 @@ namespace Symbolic_Algebra_Solver.Models
             }
         }
 
+
+        /// <summary>
+        /// Check if expression has matching opening and closing parenthesis.
+        /// </summary>
+        static private bool CheckParenthesis(string input)
+        {
+            int opening = 0, rawOpening = 0;
+            int closing = 0, rawClosing = 0;
+            foreach (char c in input)
+            {
+                if (c == '(')
+                {
+                    opening++;
+                    rawOpening++;
+                }
+                else if (c == ')')
+                {
+                    if (closing < opening)
+                    {
+                        closing++;
+                    }
+                    rawClosing++;
+                }
+            }
+
+            return (opening == closing) && (rawOpening == rawClosing);
+        }
+
         public void Simplify()
         {
+            if (!CheckParenthesis(InputExpression))
+            {
+                MessageBox.Show("Missmatching parenthesis!");
+                return;
+            }
+
             using (Py.GIL())
             {
                 OutputExpression = SympyCS.simplify(InputExpression);
@@ -29,6 +63,12 @@ namespace Symbolic_Algebra_Solver.Models
 
         public void Factor()
         {
+            if (!CheckParenthesis(InputExpression))
+            {
+                MessageBox.Show("Missmatching parenthesis!");
+                return;
+            }
+
             using (Py.GIL())
             {
                 OutputExpression = SympyCS.factor(InputExpression);
